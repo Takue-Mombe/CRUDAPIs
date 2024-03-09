5c9152fdc204b9c78e73fc78c9ef28694854bbda
mombe <h220462g@hit.ac.zw>
2024-03-09 22:28:16 +0200

    First Commit

---
+++ src/test/java/org/qr_code_generator/crudsql/CrudsqlApplicationTests.java
@@ -0,0 +1,13 @@
+package org.qr_code_generator.crudsql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudsqlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
